c2b4aafe1bbc352c637e4f83d947184787a00ec9
insertactivate <andreas.kristensson@gmail.com>
2025-04-11 20:18:25 +0200

    Revert "Fixing 404 issue at deployment with useHash."
    This reverts commit 81d5a33174cdd70a9884b4f1423e21c6961ab1aa.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
-import { NgModule } from '@angular/core';  
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
@@ -9,9 +8,3 @@ export const routes: Routes = [
 	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 	{ path: '**', redirectTo: '/dashboard' }
 ];
-
-@NgModule({  
-  imports: [RouterModule.forRoot(routes, { useHash: true })],  
-  exports: [RouterModule]  
-})  
-export class AppRoutingModule {}
