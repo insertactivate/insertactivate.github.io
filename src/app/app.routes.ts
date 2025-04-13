0e0a57af8f6f377c6e04643fc056d333bb9f0152
insertactivate <andreas.kristensson@gmail.com>
2025-04-13 19:45:53 +0200

    Basic navigation added.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,26 @@
 import { Routes } from '@angular/router';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { PublicProductsComponent } from './public-products/public-products.component';
+import { PublicChurchProductsComponent } from './public-church-products/public-church-products.component';
+import { PrivateProductsComponent } from './private-products/private-products.component';
+import { ProductionComponent } from './production/production.component';
+
+import { HistoryComponent } from './history/history.component';
+import { ContactComponent } from './contact/contact.component';
+
 
 export const routes: Routes = [
 	{ path: 'dashboard', component: DashboardComponent },
+
+	{ path: 'public/products', component: PublicProductsComponent },
+	{ path: 'public/church/products', component: PublicChurchProductsComponent },
+	{ path: 'private/products', component: PrivateProductsComponent },
+	{ path: 'production', component: ProductionComponent },
+
+	{ path: 'history', component: HistoryComponent },
+	{ path: 'contact', component: ContactComponent },
 
 	/* */
 	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
