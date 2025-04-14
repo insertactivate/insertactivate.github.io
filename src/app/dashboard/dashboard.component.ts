d0339e248e7f3d1396822b4bb3c8a09ec16d2c35
insertactivate <andreas.kristensson@gmail.com>
2025-04-14 16:25:08 +0200

    Product image assets added.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'app-dashboard',
-  imports: [ FooterComponent ],
+  imports: [ FooterComponent, RouterLink ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
